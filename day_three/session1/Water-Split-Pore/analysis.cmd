40d5c592da1a372a12186a5f5b9f11a28355fa26
Omar Khalifa <omark@br012.ib.bridges2.psc.edu>
2024-07-10 10:40:06 -0400

    Add more files to day 3 session 1

--- day_three/session1/Water-Split-Pore/analysis.cmd
+++ day_three/session1/Water-Split-Pore/analysis.cmd
@@ -1,10 +1,12 @@
 #!/bin/bash
-#SBATCH -A see220002p
+#SBATCH -A see220002p 
 #SBATCH -p RM-shared
 #SBATCH -n 8
 #SBATCH -N 1
 #SBATCH -t 2:00:00
+
+
 module load intel
 module load intel-mpi
-mpirun -n ${SLURM_JOB_CPUS_PER_NODE} ../../../shared/LAMMPS/bin/lmp_mpi -in in.water-conf-prod-nvt -v inFileName relaxed-$1H2
-O-$2.dat.out -v prodSteps 25000 -v tag water-slit-pore-n$1-$2-analysis -log log-n$1-$2-a.log
+
+mpirun -n ${SLURM_JOB_CPUS_PER_NODE} ../../../shared/LAMMPS/bin/lmp_mpi -in in.water-conf-prod-nvt -v inFileName relaxed-$1H2O-$2.dat.out -v prodSteps 25000 -v tag water-slit-pore-n$1-$2-analysis -log log-n$1-$2-a.log
